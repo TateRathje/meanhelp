1104e75e7c83e33ae95eba9f7d340ab0e370d4a7
Tate Rathje <taterathje@gmail.com>
2017-06-10 04:49:14 -0700

    added profile, more webpack config, and built bundle.js

---
+++ webpack.prod.config.js
@@ -0,0 +1,76 @@
+var webpack = require('webpack');
+var path = require('path');
+var autoprefixer = require('autoprefixer');
+var bootstrapEntryPoints = require('./webpack.bootstrap.config.js');
+
+module.exports = {
+  devtool: 'inline-source-map',
+  entry: [
+    'tether',
+     bootstrapEntryPoints.dev,
+    './client/src/config.js'
+  ],
+
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+
+  resolve: {
+    modules: ['node_modules', 'src'],
+    extensions: ['*', '.js', '.scss']
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+    jQuery: "jquery",
+    "window.jQuery": "jquery",
+    Tether: "tether",
+    "window.Tether": "tether",
+    Alert: "exports-loader?Alert!bootstrap/js/dist/alert",
+    Button: "exports-loader?Button!bootstrap/js/dist/button",
+    Carousel: "exports-loader?Carousel!bootstrap/js/dist/carousel",
+    Collapse: "exports-loader?Collapse!bootstrap/js/dist/collapse",
+    Dropdown: "exports-loader?Dropdown!bootstrap/js/dist/dropdown",
+    Modal: "exports-loader?Modal!bootstrap/js/dist/modal",
+    Popover: "exports-loader?Popover!bootstrap/js/dist/popover",
+    Scrollspy: "exports-loader?Scrollspy!bootstrap/js/dist/scrollspy",
+    Tab: "exports-loader?Tab!bootstrap/js/dist/tab",
+    Tooltip: "exports-loader?Tooltip!bootstrap/js/dist/tooltip",
+    Util: "exports-loader?Util!bootstrap/js/dist/util",
+    }),
+    new webpack.LoaderOptionsPlugin({
+      postcss: [autoprefixer],
+    }),
+  ],
+
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015']
+      }
+    }, {
+      test: /\.html$/,
+      loader: 'raw-loader'
+    }, { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
+      { test: /\.scss$/, use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'] },
+      {
+        test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: 'url-loader?limit=10000',
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
+        use: 'file-loader',
+      },
+
+      // Bootstrap 4
+      { test: /bootstrap\/dist\/js\/umd\//, use: 'imports-loader?jQuery=jquery' }],
+  },
+  
+};
